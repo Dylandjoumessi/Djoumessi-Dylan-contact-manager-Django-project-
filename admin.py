f812f4d09a7eb60d4195e52f0b6551aa7f55a90a
Dylandjoumessi <djoumessidylan2000@gmail.com>
2024-06-18 10:57:48 +0100

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Contact, Group
+
+admin.site.site_header = "Contact Manager Administration"
+admin.site.site_title = "Contact Manager Admin Portal"
+admin.site.index_title = "Welcome to the Contact Manager Admin"
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'user')
+    search_fields = ('name', 'email', 'phone_number')
+    list_filter = ('user',)
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
